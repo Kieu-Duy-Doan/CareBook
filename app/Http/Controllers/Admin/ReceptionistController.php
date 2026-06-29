656f8eb372bb5cbe62afdd63ac3a99270ec27553
phamlinh410 <oganbill12345@gmail.com>
2026-06-29 18:20:06 +0700

    sua benh nhan

--- app/Http/Controllers/Admin/ReceptionistController.php
+++ app/Http/Controllers/Admin/ReceptionistController.php
@@ -8,7 +8,8 @@ use App\Http\Requests\Admin\Validate\StoreReceptionistRequest;
 use App\Http\Requests\Admin\Validate\UpdateReceptionistRequest;
 use App\Models\User;
 use App\Models\StaffProfile;
-
+use App\Models\SystemLog;
+use Illuminate\Support\Facades\DB;
 
 class ReceptionistController extends Controller
 {
